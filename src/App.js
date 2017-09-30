bf25e4a7f01aa05f042aa186e44be63c95bf05a3
brenj <brenj@mailbox.org>
2017-09-30 17:59:32 -0500

    Add NavBar to App component

--- src/App.js
+++ src/App.js
@@ -1,27 +1,15 @@
 import React, { Component } from 'react';
 
-import { LANGUAGES } from './languages';
-import LinkList from './component/LinkList';
+import NavBar from './component/NavBar';
 
 import './App.css';
 import 'skeleton-css-webpack';
 
 class App extends Component {
   render() {
-    const links = [];
-
-    LANGUAGES.forEach((lang) => {
-      links.push([`/${lang}`.toLowerCase(), lang]);
-    })
-
     return (
-      <div className="container">
-        <div className="row">
-          <div className="two columns">Readable</div>
-          <div className="ten columns">
-            <LinkList links={links} />
-          </div>
-        </div>
+      <div className="container app">
+        <NavBar />
       </div>
     );
   }
