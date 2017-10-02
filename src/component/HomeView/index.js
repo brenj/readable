a0c41b06ece6b08074efb61a2bd613e686254086
brenj <brenj@mailbox.org>
2017-10-02 13:05:47 -0500

    Add a HomeView component to render homepage

---
+++ src/component/HomeView/index.js
@@ -0,0 +1,51 @@
+import React, { Component } from 'react';
+
+import Heading from '../Heading';
+import Post from '../Post';
+import Sorter from '../Sorter';
+
+const HEADING = "Readable is the place for discussing code snippets";
+const SUBHEADING = "- Anonymously -";
+
+class HomeView extends Component {
+  render() {
+    return (
+      <div>
+        <Heading mainText={HEADING} subText={SUBHEADING} />
+        <Sorter sorterType='post' />
+        <Post
+          post={{
+            title: 'This is a really long message and this is to test what happens when you have a really long message like this.',
+            creator: 'dude',
+            timestamp: 'June 21',
+            language: 'C#',
+            votes: '125'
+          }}
+          totalComments="15"
+        />
+        <Post
+          post={{
+            title: 'Check out this destructuring',
+            creator: 'brenj',
+            timestamp: 'June 10',
+            language: 'JavaScript',
+            votes: '19'
+          }}
+          totalComments="11"
+        />
+        <Post
+          post={{
+            title: 'And this is why I love Python',
+            creator: 'dude',
+            timestamp: 'June 21',
+            language: 'Python',
+            votes: '7'
+          }}
+          totalComments="15"
+        />
+      </div>
+    );
+  }
+}
+
+export default HomeView;
