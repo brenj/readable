55121750816be2457ed5663bfa27680d72bc1e04
brenj <brenj@mailbox.org>
2017-10-02 13:06:21 -0500

    Use react router to render HomeView

--- src/App.js
+++ src/App.js
@@ -1,53 +1,20 @@
 import React, { Component } from 'react';
+import { Route, Switch } from 'react-router-dom';
 
-import Heading from './component/Heading';
+import HomeView from './component/HomeView';
 import NavBar from './component/NavBar';
-import Post from './component/Post';
-import Sorter from './component/Sorter';
 
 import './App.css';
 import 'skeleton-css-webpack';
-
-const HEADING_MAIN = "Readable is the place for discussing code snippets";
-const HEADING_SUB = "- Anonymously -";
 
 class App extends Component {
   render() {
     return (
       <div className="container app">
         <NavBar />
-        <Heading mainText={HEADING_MAIN} subText={HEADING_SUB} />
-        <Sorter sorterType='post' />
-        <Post
-          post={{
-            title: 'This is a really long message and this is to test what happens when you have a really long message like this.',
-            creator: 'dude',
-            timestamp: 'June 21',
-            language: 'C#',
-            votes: '125'
-          }}
-          totalComments="15"
-        />
-        <Post
-          post={{
-            title: 'Check out this destructuring',
-            creator: 'brenj',
-            timestamp: 'June 10',
-            language: 'JavaScript',
-            votes: '9'
-          }}
-          totalComments="11"
-        />
-        <Post
-          post={{
-            title: 'And this is why I love Python',
-            creator: 'dude',
-            timestamp: 'June 21',
-            language: 'Python',
-            votes: '7'
-          }}
-          totalComments="15"
-        />
+        <Switch>
+          <Route exact path="/" component={HomeView} />
+        </Switch>
       </div>
     );
   }
