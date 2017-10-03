f5e9d9d37deec3eea41a071bad3307cd0ca39d77
brenj <brenj@mailbox.org>
2017-10-02 23:16:02 -0500

    Modify languages list to include more data

--- src/languages.js
+++ src/languages.js
@@ -1,11 +1,47 @@
-export const LANGUAGES = [
-  'C#',
-  'Go',
-  'Java',
-  'JS',
-  'Perl',
-  'PHP',
-  'Python',
-  'Ruby',
-  'SQL',
-];
+export const LANGUAGES = {
+  csharp: {
+    displayName: 'C#',
+    path: 'csharp',
+    tagLine: 'Structured, imperative, object-oriented, event-driven, task-driven, functional, generic, reflective, concurrent',
+  },
+  go: {
+    displayName: 'Go',
+    path: 'go',
+    tagLine: 'Go is expressive, concise, clean, and efficient'
+  },
+  java: {
+    displayName: 'Java',
+    path: 'java',
+    tagLine: 'C++ with all the knives, guns, and clubs put away'
+  },
+  javascript: {
+    displayName: 'JavaScript',
+    path: 'javascript',
+    tagLine: 'Any application that can be written in JavaScript will eventually be written in JavaScript',
+  },
+  perl: {
+    displayName: 'Perl',
+    path: 'perl',
+    tagLine: 'The only language that looks the same before and after RSA encryption',
+  },
+  php: {
+    displayName: 'PHP',
+    path: 'php',
+    tagLine: 'PHP is about as exciting as your toothbrush - Rasmus Lerdorf, creator of PHP',
+  },
+  python: {
+    displayName: 'Python',
+    path: 'python',
+    tagLine: 'Compile, run and ship your pseudo-code',
+  },
+  ruby: {
+    displayName: 'Ruby',
+    path: 'ruby',
+    tagLine: 'Ruby is a bad rip-off of Lisp or Smalltalk',
+  },
+  sql: {
+    displayName: ' SQL',
+    path: 'sql',
+    tagLine: 'Structured Query Language',
+  },
+};
