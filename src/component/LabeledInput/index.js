4a7c5bf5895b931830b0eecfd8dfb81cc66b28d6
brenj <brenj@mailbox.org>
2017-10-05 17:15:44 -0500

    Add LabeledInput component

---
+++ src/component/LabeledInput/index.js
@@ -0,0 +1,27 @@
+import React, { Component } from 'react';
+
+class LabeledInput extends Component {
+  render() {
+    const {focus, inputPlaceholder, labelText} = this.props;
+
+    return (
+      <div className="row">
+        <div className="six columns">
+          <label>{labelText}</label>
+          <input
+            autoFocus={focus}
+            className="u-full-width"
+            placeholder={inputPlaceholder}
+            type="text"
+          />
+        </div>
+      </div>
+    );
+  }
+}
+
+LabeledInput.defaultProps = {
+  focus: false,
+};
+
+export default LabeledInput;
