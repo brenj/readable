30cb9a66ec97d3486c5280f7bdb12e32a85b831e
brenj <brenj@mailbox.org>
2017-10-05 17:29:45 -0500

    Use LabeledInput for message form

--- src/component/MessageForm/index.js
+++ src/component/MessageForm/index.js
@@ -1,30 +1,30 @@
 import React, { Component } from 'react';
 
+import LabeledInput from '../LabeledInput';
+
 import './message-form.css';
 
 class MessageForm extends Component {
   render() {
     return (
       <form className="message-form">
-        <div class="row">
-          <div class="six columns">
-            <label for="nameInput">Your name</label>
-            <input
-              class="u-full-width"
-              type="text"
-              placeholder="Sadie"
-              id="nameInput"
-            />
-          </div>
-          <div class="six columns"></div>
-        </div>
-        <label for="messageTextArea">Message</label>
+        <LabeledInput
+          focus={true}
+          labelText="Name"
+          inputPlaceholder="Sadie"
+        />
+        <label>Message</label>
         <textarea
-          class="u-full-width"
+          className="u-full-width"
           placeholder="Meow, furthermore, meow mew."
-          id="messageTextArea">
+        >
         </textarea>
-        <input class="button" type="submit" value="Submit" />
+        <input className="button" type="submit" value="Cancel" />
+        <input
+          className="button message-form__button--submit"
+          type="submit"
+          value="Submit"
+        />
       </form>
     );
   }
