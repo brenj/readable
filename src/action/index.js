cf19b15b727047fb468b9de76b7421b4359e1836
brenj <brenj@mailbox.org>
2017-10-07 18:45:13 -0500

    Add showPostsByLang action

--- src/action/index.js
+++ src/action/index.js
@@ -1,4 +1,5 @@
 const SHOW_POSTS = 'SHOW_POSTS';
+const SHOW_POSTS_BY_LANG = 'SHOW_POSTS_BY_LANG';
 
 const showPosts = (posts) => {
   return {
@@ -7,4 +8,11 @@ const showPosts = (posts) => {
   };
 };
 
-export { SHOW_POSTS, showPosts };
+const showPostsByLang = (posts, language) => {
+  return {
+    type: SHOW_POSTS_BY_LANG,
+    payload: { language, posts },
+  };
+};
+
+export { SHOW_POSTS, SHOW_POSTS_BY_LANG, showPosts, showPostsByLang };
