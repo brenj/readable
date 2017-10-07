8c30094ab4c16dc76456ff8efc959788a25c01a2
brenj <brenj@mailbox.org>
2017-10-06 23:23:49 -0500

    Use react-redux provider and pass in redux store

--- src/index.js
+++ src/index.js
@@ -1,11 +1,25 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { Provider } from 'react-redux';
 import { BrowserRouter } from 'react-router-dom';
-import './index.css';
+import { createStore  } from 'redux';
+
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
 
+import './index.css';
+
+const store = createStore(
+  reducer,
+  window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+);
+
 ReactDOM.render(
-  <BrowserRouter><App /></BrowserRouter>,
+  <BrowserRouter>
+    <Provider store={store}>
+      <App />
+    </Provider>
+  </BrowserRouter>,
   document.getElementById('root'));
+
 registerServiceWorker();
