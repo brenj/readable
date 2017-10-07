7d8251d74cd0f9ef8be652327b91b43a212df5d8
brenj <brenj@mailbox.org>
2017-10-06 23:33:22 -0500

    Get posts once component has mounted
    Then dispatch showPosts action.

--- src/component/HomeView/index.js
+++ src/component/HomeView/index.js
@@ -1,14 +1,20 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
 
+import { getPosts } from '../../api';
 import Heading from '../Heading';
 import Post from '../Post';
+import { showPosts } from '../../action';
 import Sorter from '../Sorter';
 
 const HEADING = 'Readable is the place for discussing code snippets';
 const SUBHEADING = '- Anonymously -';
 
 class HomeView extends Component {
+  componentDidMount() {
+    getPosts().then(posts => this.props.dispatch(showPosts(posts)));
+  }
+
   render() {
     return (
       <div>
