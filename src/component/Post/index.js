d65c32e8b6d02a9f49de1a1a8a952bd41e19f330
brenj <brenj@mailbox.org>
2017-10-07 23:27:35 -0500

    Use react-router Links to work with Redux state

--- src/component/Post/index.js
+++ src/component/Post/index.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react';
+import { Link } from 'react-router-dom';
 
-import Link from '../Link';
 import VoteBox from '../VoteBox';
 
 import './post.css';
@@ -8,14 +8,21 @@ import './post.css';
 class Post extends Component {
   render() {
     const { post, totalComments } = this.props;
+    const postPath = `/${post.category}/${post.id}`;
 
     return (
       <VoteBox className="row post" votes={post.voteScore}>
         <div className="nine columns post__details">
-          <a className="post__title"href="/">{post.title}</a>
+          <Link className="post__title" to={postPath}>{post.title}</Link>
           <div>
             <span>by {post.author}&nbsp;</span>
-            <span>in <Link href="/" text={post.category} />&nbsp;</span>
+            <span>
+              in&nbsp;
+              <Link className="post__language" to={post.category}>
+                {post.category}
+              </Link>
+              &nbsp;
+            </span>
             <span>on {post.timestamp}&nbsp;</span>
             <span className="post__comments">
               <em>{totalComments}</em>
