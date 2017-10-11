e9a2c1887002e9d2f7376530e553b72f52fdfe5b
brenj <brenj@mailbox.org>
2017-10-11 18:28:56 -0500

    Add ADD_POST action

--- src/action/actions.js
+++ src/action/actions.js
@@ -1,3 +1,4 @@
+export const ADD_POST = 'ADD_POST';
 export const SHOW_POSTS = 'SHOW_POSTS';
 export const SHOW_POSTS_BY_LANG = 'SHOW_POSTS_BY_LANG';
 export const SHOW_POST_DETAILS = 'SHOW_POST_DETAILS';
