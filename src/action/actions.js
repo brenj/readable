a41d04367582df2317a691f3622355c73e12dd4d
brenj <brenj@mailbox.org>
2017-10-14 23:07:46 -0500

    Add edit post action

--- src/action/actions.js
+++ src/action/actions.js
@@ -1,4 +1,5 @@
 export const ADD_POST = 'ADD_POST';
+export const EDIT_POST = 'EDIT_POST';
 export const DELETE_POST = 'DELETE_POST';
 export const SHOW_POSTS = 'SHOW_POSTS';
 export const SHOW_POSTS_BY_LANG = 'SHOW_POSTS_BY_LANG';
