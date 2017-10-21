7f1d21c44ceec70e9b83a8c5acefc36425edd86a
brenj <brenj@mailbox.org>
2017-10-21 13:18:35 -0500

    Call vote handler with vote type

--- src/component/VoteBox/index.js
+++ src/component/VoteBox/index.js
@@ -4,15 +4,25 @@ import './vote-box.css';
 
 class VoteBox extends Component {
   render() {
+    const { handleVote } = this.props;
+
     return (
       <div className="row vote-box">
         <div className="three columns vote-box__votes">
-          <span>
-            <a className="vote-box__downvote" href="/">-</a>
+          <span
+            role="button"
+            className="vote-box__downvote"
+            onClick={() => handleVote('downVote')}
+          >
+            -
           </span>
           <span>{this.props.votes}</span>
-          <span>
-            <a className="vote-box__upvote" href="/">+</a>
+          <span
+            role="button"
+            className="vote-box__upvote"
+            onClick={() => handleVote('upVote')}
+          >
+            +
           </span>
         </div>
         {this.props.children}
