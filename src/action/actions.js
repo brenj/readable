70dc8bc9f4c557fb7f06a490d466a3d208afe2f7
brenj <brenj@mailbox.org>
2017-10-21 13:19:01 -0500

    Add vote on post action

--- src/action/actions.js
+++ src/action/actions.js
@@ -4,3 +4,4 @@ export const DELETE_POST = 'DELETE_POST';
 export const SHOW_POSTS = 'SHOW_POSTS';
 export const SHOW_POSTS_BY_LANG = 'SHOW_POSTS_BY_LANG';
 export const SHOW_POST_DETAILS = 'SHOW_POST_DETAILS';
+export const VOTE_ON_POST = 'VOTE_ON_POST';
