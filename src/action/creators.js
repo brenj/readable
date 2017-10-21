bbad2806719c07dfa7dff32fcac2672be65eb7db
brenj <brenj@mailbox.org>
2017-10-21 13:19:41 -0500

    Add voteOnPost action creator

--- src/action/creators.js
+++ src/action/creators.js
@@ -29,3 +29,8 @@ export const showPostDetails = (post, comments) => ({
   type: actions.SHOW_POST_DETAILS,
   payload: { comments, post },
 });
+
+export const voteOnPost = (post, voteType) => ({
+  type: actions.VOTE_ON_POST,
+  payload: { post, voteType },
+});
