9fa987d22d0b5dc1d71a0d99264d5c5683b2fee7
brenj <brenj@mailbox.org>
2017-11-04 10:26:51 -0500

    Add a delete comment action

--- src/action/actions.js
+++ src/action/actions.js
@@ -1,7 +1,8 @@
 export const ADD_COMMENT = 'ADD_COMMENT';
 export const ADD_POST = 'ADD_POST';
-export const EDIT_POST = 'EDIT_POST';
+export const DELETE_COMMENT = 'DELETE_COMMENT';
 export const DELETE_POST = 'DELETE_POST';
+export const EDIT_POST = 'EDIT_POST';
 export const SHOW_POSTS = 'SHOW_POSTS';
 export const SHOW_POSTS_BY_LANG = 'SHOW_POSTS_BY_LANG';
 export const SHOW_POST_DETAILS = 'SHOW_POST_DETAILS';
