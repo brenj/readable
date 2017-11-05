523e783c7e4c61fa0d4d9419c6ecdf3786131ac3
brenj <brenj@mailbox.org>
2017-11-04 22:13:08 -0500

    Pass edit handler through comment lister

--- src/component/CommentLister/index.js
+++ src/component/CommentLister/index.js
@@ -3,12 +3,16 @@ import React from 'react';
 import Comment from '../Comment';
 
 function CommentLister(props) {
-  const { comments } = props;
+  const { comments, editHandler } = props;
 
   return (
     <div>
       {comments.map(comment => (
-        <Comment key={comment.id} comment={comment} />))}
+        <Comment
+          key={comment.id}
+          comment={comment}
+          editHandler={editHandler}
+        />))}
     </div>
   );
 }
