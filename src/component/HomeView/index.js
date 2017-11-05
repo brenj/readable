88dea91f1be1d4b3e9693b0a116c355116948259
brenj <brenj@mailbox.org>
2017-11-05 00:11:32 -0500

    Update add post button in home view

--- src/component/HomeView/index.js
+++ src/component/HomeView/index.js
@@ -1,11 +1,14 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
+import { Link } from 'react-router-dom';
 
 import { getPosts } from '../../api/methods';
 import Heading from '../Heading';
 import PostLister from '../PostLister';
 import { showPosts } from '../../action/creators';
 import Sorter from '../Sorter';
+
+import './home-view.css';
 
 const HEADING = 'Readable is the place for discussing code snippets';
 const SUBHEADING = '- Anonymously -';
@@ -21,6 +24,13 @@ class HomeView extends Component {
     return (
       <div>
         <Heading mainText={HEADING} subText={SUBHEADING} />
+        <Link to="/post/new">
+          <button
+            className="button-primary home-view__button--comment"
+          >
+            Add Post
+          </button>
+        </Link>
         <Sorter sorterType="post" />
         <PostLister posts={posts} comments={comments} />
       </div>
