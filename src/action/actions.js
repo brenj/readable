40caa2ae042b7b3fa9b611183e9096d6673584c8
brenj <brenj@mailbox.org>
2017-11-04 22:03:23 -0500

    Add edit comment action

--- src/action/actions.js
+++ src/action/actions.js
@@ -2,6 +2,7 @@ export const ADD_COMMENT = 'ADD_COMMENT';
 export const ADD_POST = 'ADD_POST';
 export const DELETE_COMMENT = 'DELETE_COMMENT';
 export const DELETE_POST = 'DELETE_POST';
+export const EDIT_COMMENT = 'EDIT_COMMENT';
 export const EDIT_POST = 'EDIT_POST';
 export const SHOW_POSTS = 'SHOW_POSTS';
 export const SHOW_POSTS_BY_LANG = 'SHOW_POSTS_BY_LANG';
