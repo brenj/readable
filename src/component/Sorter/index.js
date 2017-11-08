a5109fb392544d94fb086a1f12f1a7377c362f21
brenj <brenj@mailbox.org>
2017-11-08 00:01:30 -0600

    Update sorter component
    Change class name and click handler depending on which sort option was
    clicked: vote or date.

--- src/component/Sorter/index.js
+++ src/component/Sorter/index.js
@@ -4,17 +4,45 @@ import './sorter.css';
 
 class Sorter extends Component {
   render() {
-    const { sorterType } = this.props;
+    const { activeSort, activeSortHandler, sorterType } = this.props;
 
     return (
       <div className="row sorter">
         <div className="one column" />
-        <div className="eleven columns sorter__heading">
+        <div className="eleven columns">
           <span>
             Sort {`${sorterType}s`} by&nbsp;
-            <a href="/">Popularity</a>
+            <span
+              role="button"
+              className={
+                activeSort === 'vote' ?
+                  "sorter__sort--inactive" :
+                  "sorter__sort--active"
+              }
+              onClick={() => {
+                if (activeSort !== "vote") {
+                  activeSortHandler('vote');
+                }
+              }}
+            >
+              Vote
+            </span>
             &nbsp;or&nbsp;
-            <a href="/">Date</a>
+            <span
+              role="button"
+              className={
+                activeSort === 'date' ?
+                  "sorter__sort--inactive" :
+                  "sorter__sort--active"
+              }
+              onClick={() => {
+                if (activeSort !== "date") {
+                  activeSortHandler('date');
+                }
+              }}
+            >
+              Date
+            </span>
           </span>
         </div>
       </div>
