48785277049d29d11a3d25a4bb07666f40176880
brenj <brenj@mailbox.org>
2017-11-09 14:27:17 -0600

    Add propTypes to Sorter component

--- src/component/Sorter/index.js
+++ src/component/Sorter/index.js
@@ -1,53 +1,62 @@
-import React, { Component } from 'react';
+import PropTypes from 'prop-types';
+import React from 'react';
 
 import './sorter.css';
 
-class Sorter extends Component {
-  render() {
-    const { activeSort, activeSortHandler, sorterType } = this.props;
+const propTypes = {
+  activeSort: PropTypes.string.isRequired,
+  activeSortHandler: PropTypes.func.isRequired,
+  sorterType: PropTypes.string.isRequired,
+};
 
-    return (
-      <div className="row sorter">
-        <div className="one column" />
-        <div className="eleven columns">
-          <span>
-            Sort {`${sorterType}s`} by&nbsp;
-            <span
-              role="button"
-              className={
-                activeSort === 'VOTE' ?
-                  'sorter__sort--inactive' :
-                  'sorter__sort--active'
+function Sorter(props) {
+  const { activeSort, activeSortHandler, sorterType } = props;
+
+  return (
+    <div className="row sorter">
+      <div className="one column" />
+      <div className="eleven columns">
+        <span>
+          Sort {`${sorterType}s`} by&nbsp;
+          <span
+            className={
+              activeSort === 'VOTE' ?
+                'sorter__sort--inactive' :
+                'sorter__sort--active'
+            }
+            onClick={() => {
+              if (activeSort !== 'VOTE') {
+                activeSortHandler('VOTE');
               }
-              onClick={() => {
-                if (activeSort !== 'VOTE') {
-                  activeSortHandler('VOTE');
-                }
-              }}
-            >
-              Vote
-            </span>
-            &nbsp;or&nbsp;
-            <span
-              role="button"
-              className={
-                activeSort === 'DATE' ?
-                  'sorter__sort--inactive' :
-                  'sorter__sort--active'
-              }
-              onClick={() => {
-                if (activeSort !== 'DATE') {
-                  activeSortHandler('DATE');
-                }
-              }}
-            >
-              Date
-            </span>
+            }}
+            role="button"
+            tabIndex="0"
+          >
+            Vote
           </span>
-        </div>
+          &nbsp;or&nbsp;
+          <span
+            className={
+              activeSort === 'DATE' ?
+                'sorter__sort--inactive' :
+                'sorter__sort--active'
+            }
+            onClick={() => {
+              if (activeSort !== 'DATE') {
+                activeSortHandler('DATE');
+              }
+            }}
+            role="button"
+            tabIndex="-1"
+          >
+            Date
+          </span>
+        </span>
       </div>
-    );
-  }
+    </div>
+  );
 }
+
+Sorter.propTypes = propTypes;
 
 export default Sorter;
