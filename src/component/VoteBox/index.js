b48ff0968c620cc2a4d031cd923b58006704f8f4
brenj <brenj@mailbox.org>
2017-11-09 09:05:51 -0600

    Cleanup VoteBox component

--- src/component/VoteBox/index.js
+++ src/component/VoteBox/index.js
@@ -1,34 +1,43 @@
-import React, { Component } from 'react';
+import PropTypes from 'prop-types';
+import React from 'react';
 
 import './vote-box.css';
 
-class VoteBox extends Component {
-  render() {
-    const { handleVote } = this.props;
+const propTypes = {
+  children: PropTypes.arrayOf(PropTypes.object).isRequired,
+  handleVote: PropTypes.func.isRequired,
+  votes: PropTypes.number.isRequired,
+};
 
-    return (
-      <div className="row vote-box">
-        <div className="three columns vote-box__votes">
-          <span
-            role="button"
-            className="vote-box__downvote"
-            onClick={() => handleVote('downVote')}
-          >
-            -
-          </span>
-          <span>{this.props.votes}</span>
-          <span
-            role="button"
-            className="vote-box__upvote"
-            onClick={() => handleVote('upVote')}
-          >
-            +
-          </span>
-        </div>
-        {this.props.children}
+function VoteBox(props) {
+  const { children, handleVote, votes } = props;
+
+  return (
+    <div className="row vote-box">
+      <div className="three columns vote-box__votes">
+        <span
+          className="vote-box__downvote"
+          onClick={() => handleVote('downVote')}
+          role="button"
+          tabIndex="0"
+        >
+          -
+        </span>
+        <span>{votes}</span>
+        <span
+          className="vote-box__upvote"
+          onClick={() => handleVote('upVote')}
+          role="button"
+          tabIndex="-1"
+        >
+          +
+        </span>
       </div>
-    );
-  }
+      {children}
+    </div>
+  );
 }
+
+VoteBox.propTypes = propTypes;
 
 export default VoteBox;
