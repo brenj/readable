7e09b1a87c330a585726ab4dcd627ade37b08775
brenj <brenj@mailbox.org>
2017-11-09 23:29:20 -0600

    Transform NavBar into functional component

--- src/component/NavBar/index.js
+++ src/component/NavBar/index.js
@@ -1,32 +1,30 @@
-import React, { Component } from 'react';
+import React from 'react';
 
 import { getLanguage, getLanguages } from '../../languages';
 import NavLinks from '../../component/NavLinks';
 
 import './navbar.css';
 
-class NavBar extends Component {
-  render() {
-    const links = [];
+function NavBar() {
+  const links = [];
 
-    getLanguages().forEach((langKey) => {
-      const { displayName, path } = getLanguage(langKey);
-      links.push(
-        { href: `/language/${path}`.toLowerCase(), text: displayName });
-    });
+  getLanguages().forEach((langKey) => {
+    const { displayName, path } = getLanguage(langKey);
+    links.push(
+      { href: `/language/${path}`.toLowerCase(), text: displayName });
+  });
 
-    return (
-      <div className="row navbar">
-        <div className="two columns">
-          <strong className="navbar__strong">Readable</strong>
-          <span className="navbar__span">&nbsp;&lt;code&gt;</span>
-        </div>
-        <div className="ten columns">
-          <NavLinks links={links} />
-        </div>
+  return (
+    <div className="row navbar">
+      <div className="two columns">
+        <strong className="navbar__strong">Readable</strong>
+        <span className="navbar__span">&nbsp;&lt;code&gt;</span>
       </div>
-    );
-  }
+      <div className="ten columns">
+        <NavLinks links={links} />
+      </div>
+    </div>
+  );
 }
 
 export default NavBar;
