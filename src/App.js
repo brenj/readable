946c79c7ba3bac7caeac763c6951e4d4c4badf00
brenj <brenj@mailbox.org>
2017-11-10 08:57:01 -0600

    Add prop types to App

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React, { Component } from 'react';
+import React from 'react';
 import { Route, Switch } from 'react-router-dom';
 
 import 'skeleton-css-webpack'; // eslint-disable-line import/extensions
@@ -9,33 +9,30 @@ import PostForm from './component/PostForm';
 import PostView from './component/PostView';
 
 import './App.css';
-import 'skeleton-css-webpack';
 
-class App extends Component {
-  render() {
-    return (
-      <div className="container app">
-        <NavBar />
-        <Switch>
-          <Route exact path="/" component={HomeView} />
-          <Route exact path="/language/:lang" component={LanguageView} />
-          <Route
-            exact
-            path="/post/new"
-            render={props => (
-              <PostForm {...props} formType="add" heading="Add a post" />)}
-          />
-          <Route
-            exact
-            path="/post/edit/:id"
-            render={props => (
-              <PostForm {...props} formType="edit" heading="Edit a post" />)}
-          />
-          <Route exact path="/post/:id" component={PostView} />
-        </Switch>
-      </div>
-    );
-  }
+function App() {
+  return (
+    <div className="container app">
+      <NavBar />
+      <Switch>
+        <Route exact path="/" component={HomeView} />
+        <Route exact path="/language/:lang" component={LanguageView} />
+        <Route
+          exact
+          path="/post/new"
+          render={props => (
+            <PostForm {...props} formType="add" heading="Add a post" />)}
+        />
+        <Route
+          exact
+          path="/post/edit/:id"
+          render={props => (
+            <PostForm {...props} formType="edit" heading="Edit a post" />)}
+        />
+        <Route exact path="/post/:id" component={PostView} />
+      </Switch>
+    </div>
+  );
 }
 
 export default App;
