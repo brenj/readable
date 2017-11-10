84a299739a2dc221af46a50a7c665ab8f79e5e00
brenj <brenj@mailbox.org>
2017-11-09 23:56:54 -0600

    Add prop types for LabeledSelect

--- src/component/LabeledSelect/index.js
+++ src/component/LabeledSelect/index.js
@@ -1,40 +1,54 @@
-import React, { Component } from 'react';
+import PropTypes from 'prop-types';
+import React from 'react';
 
-class LabeledSelect extends Component {
-  render() {
-    const {
-      id,
-      labelText,
-      name,
-      onChange,
-      options,
-      value,
-    } = this.props;
+const propTypes = {
+  id: PropTypes.string.isRequired,
+  labelText: PropTypes.string.isRequired,
+  name: PropTypes.string.isRequired,
+  onChange: PropTypes.func.isRequired,
+  options: PropTypes.shape({
+    text: PropTypes.string.isRequired,
+    value: PropTypes.string.isRequired,
+    tagLine: PropTypes.string.isRequired,
+  }).isRequired,
+  value: PropTypes.func.isRequired,
+};
 
-    return (
-      <div className="row">
-        <div className="six columns">
-          <label htmlFor={id}>{labelText}</label>
-          <select
-            className="u-full-width"
-            id={id}
-            onChange={onChange}
-            name={name}
-            value={value}
-          >
-            {options.map(option => (
-              <option
-                key={option.value}
-                value={option.value}
-              >
-                {option.text}
-              </option>
-            ))}
-          </select>
-        </div>
+function LabeledSelect(props) {
+  const {
+    id,
+    labelText,
+    name,
+    onChange,
+    options,
+    value,
+  } = props;
+
+  return (
+    <div className="row">
+      <div className="six columns">
+        <label htmlFor={id}>{labelText}</label>
+        <select
+          className="u-full-width"
+          id={id}
+          onChange={onChange}
+          name={name}
+          value={value}
+        >
+          {options.map(option => (
+            <option
+              key={option.value}
+              value={option.value}
+            >
+              {option.text}
+            </option>
+          ))}
+        </select>
       </div>
-    );
-  }
+    </div>
+  );
 }
+
+LabeledSelect.propTypes = propTypes;
 
 export default LabeledSelect;
