04912a3b7428ae33bf2c8529a51cd50508435ec0
brenj <brenj@mailbox.org>
2017-11-23 11:39:26 -0600

    Load posts when application is rendered
    This will allow Redux state to be used more effectively and efficiently.

--- src/index.js
+++ src/index.js
@@ -1,15 +1,16 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
+import { creators } from './action';
 import Root from './component/Root';
 import registerServiceWorker from './registerServiceWorker';
 import configureStore from './store';
 
 import './index.css';
 
-ReactDOM.render(
-  <Root store={configureStore()} />,
-  document.getElementById('root'),
-);
+const store = configureStore();
 
+ReactDOM.render(<Root store={store} />, document.getElementById('root'));
+
+store.dispatch(creators.loadPosts());
 registerServiceWorker();
